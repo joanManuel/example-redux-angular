f573c1c746cfbe537ae74146fa73dc5d60843ce5
joanManuel <joan.torres.jt@gmail.com>
2020-08-22 19:20:44 -0500

    Implementando redux en el componente padre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as actions from './contador/contador.actions';
+
+interface AppState {
+  contador: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,15 +14,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'redux-app';
   contador: number;
-  constructor() {
-    this.contador = 10;
+  constructor(private store: Store<AppState>) {
+    this.store.select('contador').subscribe((contador) => {
+      this.contador = contador;
+    });
   }
 
   incrementar() {
-    this.contador++;
+    this.store.dispatch(actions.incrementar());
   }
 
   decrementar() {
-    this.contador--;
+    this.store.dispatch(actions.decrementar());
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 import { AppComponent } from './app.component';
 import { HijoComponent } from './contador/hijo/hijo.component';
 import { NietoComponent } from './contador/nieto/nieto.component';
 
+// NgRx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { contadorReducer } from './contador/contador.reducer';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    HijoComponent,
-    NietoComponent
-  ],
+  declarations: [AppComponent, HijoComponent, NietoComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ contador: contadorReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
